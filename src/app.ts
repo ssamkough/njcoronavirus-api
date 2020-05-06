22956264a40a20ebf61a21bbcd44bad3a8c61fd4
Sammy Samkough <sammysamkough@gmail.com>
2020-05-06 04:53:28 -0400

    Added cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 require('dotenv').config();
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 import router from './router';
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 app.use('/api', router);
 
 app.get('/', (req, res) => {
