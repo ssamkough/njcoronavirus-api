b04ac6e0f3988c8a1e254b577832f2022d8dcc06
Sammy Samkough <sammysamkough@gmail.com>
2020-05-09 22:24:42 -0400

    Added redis client to express middleware

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,16 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
+import redisClient from './middleware/cache/redisClient';
+import cache from './middleware/cache/cache';
 import router from './router';
 
 const app = express();
+app.use(redisClient);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
-app.use('/api', router);
+app.use('/api', cache, router);
 
 app.get('/', (req, res) => {
     const fullUrl: string = req.protocol + '://' + req.get('host') + req.originalUrl + 'api';
