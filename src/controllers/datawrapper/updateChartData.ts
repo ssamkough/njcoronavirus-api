ad80af796956c03900c0a502d46e03d0faafff14
Sammy Samkough <sammysamkough@gmail.com>
2020-05-09 22:25:47 -0400

    Commented out code

--- src/controllers/datawrapper/updateChartData.ts
+++ src/controllers/datawrapper/updateChartData.ts
@@ -1,20 +1,22 @@
 import axios from 'axios';
 
 const updateChartData = async (req, res, next) => {
-    const id = req.params.id;
-    const chartData = req.body.data;
-    const url: string = `https://api.datawrapper.de/v3/charts/${id}/data`;
+    console.log(req.body);
+    // const id: number = req.params.id;
+    // const fileName: string = `${req.params.id}_csv`;
+    // const chartData = req.body.data;
+    // const url: string = `https://api.datawrapper.de/v3/charts/${id}/data`;
 
-    const chart = await axios.put(url, {
-        headers: {
-            Authorization: `Bearer ${process.env.DATAWRAPPER_API}`,
-        },
-        data: {
-            chartData,
-        },
-    });
+    // const chart = await axios.put(url, {
+    //     headers: {
+    //         Authorization: `Bearer ${process.env.DATAWRAPPER_API}`,
+    //     },
+    //     data: {
+    //         chartData,
+    //     },
+    // });
 
-    res.json(render(chart.data));
+    // res.json(render(chart.data));
 };
 
 const render = (chart) => {
